93b1a202aa9889aa0c76eb89379d11f7603f7447
veenapromantia <veena.h@promantia.com>
2023-10-20 13:48:24 +0530

    17082/File url validation error

--- frappe_s3_attachment/__init__.py
+++ frappe_s3_attachment/__init__.py
@@ -3,3 +3,10 @@ from __future__ import unicode_literals
 
 __version__ = '0.0.1'
 
+import frappe
+import frappe.core.doctype.file.file
+import frappe_s3_attachment.frappe_s3_attachment.doctype.s3_file_attachment.s3_file_attachment as _custom
+
+print('DSSAA____------')
+frappe.core.doctype.file.file.File.validate_file_url = _custom.validate_file_url
+
